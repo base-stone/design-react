a9b911af7ad38930faef833a78d214ea35d45f58
base-stone <leafront@126.com>
2023-12-15 16:23:49 +0800

    update empty

--- src/components/empty/index.tsx
+++ src/components/empty/index.tsx
@@ -9,11 +9,7 @@ const Empty: FC<Props> = memo((props) => {
   const { image, title = '暂无数据' } = props
   return (
     <div className="ui-empty flex-column flex-v-center flex-h-center">
-      {image ? (
-        <div className="ui-empty-pic" style={{ backgroundImage: `url(${image})` }}></div>
-      ) : (
-        <div className="ui-empty-pic"></div>
-      )}
+      <div className="ui-empty-pic" style={{backgroundImage: image ? `url(${image})` : 'none'}}></div>
       <span className="lh44">{title}</span>
     </div>
   )
